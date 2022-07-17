15f879fdaf64fb2991eed0fe466e425280f5c9c6
chigov2 <chigov@gmail.com>
2022-07-17 15:09:43 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_data_rest1707_2/SpringDataRest17072Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_rest1707_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRest17072Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRest17072Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_data_rest1707_2/SpringDataRest17072ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_rest1707_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRest17072ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
